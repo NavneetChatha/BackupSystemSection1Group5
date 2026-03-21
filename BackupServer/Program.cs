5bba4f3ed9e2be484ec9be23039a84c2507fb9a3
NavneetChatha <nchatha9769@conestogac.on.ca>
2026-03-21 08:09:32 -0400

    Add client and server projects

---
+++ BackupServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
